bbcfa31e1d81eaf25d6db97070dbe3d985107e51
Şafak Tamses <safak.tamses@semper.tech.com>
2022-11-16 22:20:52 -0800

    15.11 upload
    ...

---
+++ ClusteringMethod.py
@@ -0,0 +1,100 @@
+from kneed import KneeLocator
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def dataPre():
+
+    dataset = pd.read_csv('50_Startups.csv')
+    X = dataset.iloc[:, 0:3 ].values
+    y = dataset.iloc[:, -1].values
+    return X, y
+
+
+def featureScale():
+    X, y = dataPre()
+    # Future Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    return X,y
+
+
+#to find the number of clusters
+def elbowMethod():
+    X, y = featureScale()
+    from sklearn.cluster import KMeans
+    wcss = []
+    for i in range(1, 30):
+        kmeans = KMeans(n_clusters=i, init='k-means++', random_state=1)
+        kmeans.fit(X)
+        wcss.append(kmeans.inertia_)
+    x = range(0, len(wcss))
+    kn = KneeLocator(x, wcss, curve='convex', direction='decreasing')
+    plt.xlabel('number of clusters k')
+    plt.ylabel('Sum of squared distances')
+    plt.plot(x, wcss, 'bx-')
+    plt.vlines(kn.knee, plt.ylim()[0], plt.ylim()[1], linestyles='dashed')
+    plt.show()
+    return kn.knee
+
+#to find the number of clusters
+def dendrogram():
+    X,y = featureScale()
+    import scipy.cluster.hierarchy as sch
+    dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+
+    plt.title('Dendrogram')
+    plt.xlabel('Customers')
+    plt.ylabel('Euclidean distances')
+    plt.show()
+
+def kMeansClustering():
+    X,y = featureScale()
+    numberOfClusters = elbowMethod()
+    from sklearn.cluster import KMeans
+    kmeans = KMeans(n_clusters=numberOfClusters , init='k-means++', random_state= 2)
+    kmeans.fit(X)
+    y_kmeans = kmeans.fit_predict(X)
+    # Visualising the clusters
+    color = ['r', 'b', 'g', 'c', 'm','k']
+    for k in range(numberOfClusters):
+        arrayDot = X[y_kmeans == k, 0], X[y_kmeans == k, 1]
+        plt.scatter(X[y_kmeans == k, 0], X[y_kmeans == k, 1], s=10, c=color[k % 6])
+        plt.scatter((kmeans.cluster_centers_[k][0]),(kmeans.cluster_centers_[k][1]) , s=20 , c=color[k % 6])
+        for i in range(len(arrayDot[0])):
+            x1 = []
+            y1 = []
+            x2 = []
+            y2 = []
+            x2.append(arrayDot[0][i])
+            y2.append(arrayDot[1][i])
+            x1.append(kmeans.cluster_centers_[k][0])
+            y1.append(kmeans.cluster_centers_[k][1])
+            x3 = x1 + x2
+            y3 = y1 + y2
+            plt.plot(x3,y3, color[k % 6])
+    plt.title('  ')
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+def hierarchicalClustering():
+    X,y = featureScale()
+    numberOfClusters = elbowMethod()
+    from sklearn.cluster import AgglomerativeClustering
+    hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+    y_hc = hc.fit_predict(X)
+    plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s=100, c='red', label='Cluster 1')
+    plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s=100, c='blue', label='Cluster 2')
+    plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s=100, c='green', label='Cluster 3')
+    plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s=100, c='cyan', label='Cluster 4')
+    plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s=100, c='magenta', label='Cluster 5')
+    plt.title('Clusters of customers')
+    plt.xlabel('Annual Income (k$)')
+    plt.ylabel('Spending Score (1-100)')
+    plt.legend()
+    plt.show()
+
+
+
+hierarchicalClustering()
---
+++ dataTypeTest.py
@@ -0,0 +1,38 @@
+s = "Hello"
+print(type(s))
+# <class 'str'>
+
+i = 3
+print(type(i))
+# <class 'int'>
+
+f = 10.1
+print(type(f))
+# <class 'float'>
+
+c = 3 + 4j
+print(type(c))
+# <class 'complex'>
+
+t = (("apple","banana","cherry"))
+print(type(t))
+# <class 'tuple'>
+
+l = list(("apple","banana","cherry"))
+print(type(l))
+# <class 'list'>
+
+r = range(7)
+print(type(r))
+# <class 'range'>
+
+d = dict(name="John",age=36)
+print(type(d))
+# <class 'dict'>
+
+s = set(("apple","banana","cherry"))
+print(type(s))
+# <class 'set'>
+
+
+
